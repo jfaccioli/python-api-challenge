82777b1d6d9f6ea86b893a8a479f03a6a35fc079
Jimmy Faccioli <77733975+jfaccioli@users.noreply.github.com>
2021-04-27 16:51:53 +0800

    upload new work

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "your key"
+weather_api_key = "e52c4b53fd8f54a95f21172a86102519"
 
 # Google API Key
-g_key = "your key"
+g_key = "AIzaSyBGrDUc2uKjCacVYC8YIkO5AAnKTUpc3jU"
